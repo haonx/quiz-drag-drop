6b1c30ca7e7eab58aae3659542daceb323d24afd
haonx <qtvhao@gmail.com>
2018-01-02 20:17:02 +0700

    split placeholder and answers to segments

--- quiz-drag-drop.js
+++ quiz-drag-drop.js
@@ -4,6 +4,24 @@ angular
         return {
             link: function ($scope) {
                 var scope = $scope;
+                var placeholderSign = 'placeholderSegment';
+                scope.placeholderSegments = scope.placeholder.replace(/{\d}/gi, placeholderSign).split(placeholderSign);
+
+                scope.segmentsCollection = [scope.placeholderSegments, scope.answers];
+                scope.lengths = [];
+                for(var i = 0; i<scope.segmentsCollection.length;i++) {
+                    scope.lengths.push(scope.segmentsCollection[i].length);
+                }
+
+                scope.segments = [];
+                var maxLength = Math.max.apply(null,scope.lengths);
+                for(var k = 0; k<maxLength; k++){
+                    for(var j = 0; j<scope.segmentsCollection.length;j++) {
+                        if (scope.segmentsCollection[j]) {
+                            scope.segments.push(scope.segmentsCollection[j][k]);
+                        }
+                    }
+                }
             },
             templateUrl: "drag-drop-quiz.html",
             scope: {
@@ -11,4 +29,10 @@ angular
                 answers: "="
             }
         };
-    });
+    })
+    .filter('typeof', function () {
+        return function (value, wordwise, max, tail) {
+            return typeof value;
+        }
+    })
+;
